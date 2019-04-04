73d572adb751364887cb2f4da86160fa2dc7737a
Unknown <unknown@example.com>
2019-04-04 10:35:16 +0200

    tp3

--- content.php
+++ content.php
@@ -1,5 +1,8 @@
 <div class="blog-post">
- <h2 class="blog-post-itle"><?php the_title(); ?></h2>
+<h2 class="blog-post-title"><a href="<?php comments_link(); ?>"> <?php printf( _nx( 'One Comment',
+'%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ),
+number_format_i18n( get_comments_number() ) ); ?> </a></h2>
  <p class="blog-post-meta"><?php the_date(); ?> par <a href="#"><?php the_author(); ?></a></p>
- <?php the_content(); ?>
+<?php the_excerpt(); ?>
 </div><!-- /.blog-post -->
+
